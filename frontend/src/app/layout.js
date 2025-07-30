20fd084d782f84f63a7ed51a3cbda06307e35665
Daniel Enriquez <danicraft.op7@gmail.com>
2025-07-30 16:23:37 -0500

    fix testimonial video bugs

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -21,6 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=imagesearch_roller"
